5facbd63a534f05f9cd0feffb3f09d8092b8fa51
Youssef Mohamed <youssef.mu.saber@gmail.com>
2023-07-04 13:54:19 +0300

    Created the bloc initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:api_request/layouts/news_layout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-          child: Text('asd'),
-        ),
+        body: NewsLayout()
       ),
     );
   }
---
+++ lib/shared/cubit/cubit.dart
@@ -0,0 +1,8 @@
+import 'package:api_request/shared/cubit/states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NewsCubit extends Cubit<NewsStates> {
+  NewsCubit() : super(NewsInitialState());
+
+  static NewsCubit get(context) => BlocProvider.of(context);
+}

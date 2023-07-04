a1b31d3334d649a8ad4c95a19b0ff5ccf4156dbe
Youssef Mohamed <youssef.mu.saber@gmail.com>
2023-07-04 14:16:41 +0300

    Created the bottomNavigationBar

--- lib/layouts/news_layout.dart
+++ lib/layouts/news_layout.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
 class NewsLayout extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Text('asd'),
+    return Scaffold(
+      body: const SafeArea(
+        child: Text('asd'),
+      ),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.white70,
+        onDestinationSelected: (index) {},
+        destinations: const [
+          NavigationDestination(
+              selectedIcon: Icon(
+                Icons.business_center_outlined,
+                color: Colors.blueAccent,
+              ),
+              icon: Icon(
+                Icons.business_center_outlined,
+                color: Colors.grey,
+              ),
+              label: 'Business'),
+          NavigationDestination(
+              selectedIcon: Icon(
+                Icons.science_outlined,
+                color: Colors.blueAccent,
+              ),
+              icon: Icon(
+                Icons.science_outlined,
+                color: Colors.grey,
+              ),
+              label: 'Science'),
+          NavigationDestination(
+              selectedIcon: Icon(
+                Icons.sports_football_outlined,
+                color: Colors.blueAccent,
+              ),
+              icon: Icon(
+                Icons.sports_football_outlined,
+                color: Colors.grey,
+              ),
+              label: 'Sports'),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: NewsLayout()
-      ),
+      home: NewsLayout()
     );
   }
 }
